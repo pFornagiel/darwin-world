bf287482f9aa5b9b65fa507d9531e05dc09f48e3
Massny <pawel.fornagiel2@gmail.com>
2024-12-23 17:29:59 +0100

    changes due to renaming the project package

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,6 @@ module org.example.darwinworld {
   requires javafx.fxml;
 
 
-  opens org.example.darwinworld to javafx.fxml;
-  exports org.example.darwinworld;
+  opens agh.ics.oop to javafx.fxml;
+  exports agh.ics.oop;
 }
