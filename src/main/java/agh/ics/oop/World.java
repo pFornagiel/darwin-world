f2570fc8d0318efb03a1fe1e63a5bfd6f8e46d60
Massny <pawel.fornagiel2@gmail.com>
2024-12-28 15:50:45 +0100

    add temporary test of new functionality

--- src/main/java/agh/ics/oop/World.java
+++ src/main/java/agh/ics/oop/World.java
@@ -5,10 +5,9 @@ import agh.ics.oop.model.simulation.SimulationEngine;
 import agh.ics.oop.model.util.MoveDirection;
 import agh.ics.oop.model.util.OptionsParser;
 import agh.ics.oop.model.util.Vector2d;
-import agh.ics.oop.model.worldmap.AbstractWorldMap;
+import agh.ics.oop.model.worldmap.BaseWorldMap;
 import agh.ics.oop.model.worldmap.ConsoleMapDisplay;
-import agh.ics.oop.model.worldmap.GrassField;
-import agh.ics.oop.model.worldmap.RectangularMap;
+import agh.ics.oop.model.worldmap.WorldMap;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -16,46 +15,8 @@ import java.util.Collections;
 public class World {
   public static void main(String[] args) {
 
-    ConsoleMapDisplay consoleMapDisplay = new ConsoleMapDisplay();
-
-    ArrayList<AbstractWorldMap> maps = new ArrayList<>();
-    int amountOfMaps = 100;
-    for(int i = 0; i< amountOfMaps; i++){
-      AbstractWorldMap newMap;
-      if(i%2 == 0){
-        newMap = new RectangularMap(10 + i /10, 10 + i/10);
-      } else {
-        newMap = new GrassField(5 + i/10);
-      }
-      newMap.addToListeners(consoleMapDisplay);
-      maps.add(newMap);
-    }
-
-    String[] arguments = {"l", "r", "b", "f","f","f","r","r","b","f"};
-
-    ArrayList<Vector2d> positionList = new ArrayList<>();
-    Collections.addAll(
-        positionList,
-        new Vector2d(2,2),
-        new Vector2d(1,1),
-        new Vector2d(10,10)
-    );
-    ArrayList<MoveDirection> directionList = OptionsParser.parse(arguments);
-
-    ArrayList<Simulation> simulationList = new ArrayList<>();
-    for(AbstractWorldMap map: maps){
-      simulationList.add(new Simulation(positionList,directionList,map));
-    }
-
-    SimulationEngine simulationEngine = new SimulationEngine(simulationList);
-    try{
-      simulationEngine.runAsyncInThreadPool();
-      simulationEngine.awaitSimulationEnd();
-    } catch (InterruptedException exception){
-      System.out.printf("%s%n", exception.getMessage());
-    }
-    System.out.println("System has finished");
-
+    BaseWorldMap map = new BaseWorldMap(20,20, 20);
+    System.out.println(map.toString());
 
 }
 
