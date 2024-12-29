b4fc8cb298bb446c20e9160cc49cca0f84ee98aa
Massny <pawel.fornagiel2@gmail.com>
2024-12-29 21:55:42 +0100

    add temporary tests

--- src/main/java/agh/ics/oop/World.java
+++ src/main/java/agh/ics/oop/World.java
@@ -14,24 +14,10 @@ import java.util.Collections;
 
 public class World {
   public static void main(String[] args) {
-
-    BaseWorldMap map = new BaseWorldMap(20,20, 20);
-    System.out.println(map.toString());
-
-}
-
-  private static void run(ArrayList<MoveDirection> args){
-    for (MoveDirection arg : args) {
-      printMessage(arg);
-    }
-  }
-
-  private static void printMessage(MoveDirection arg) {
-    switch (arg) {
-      case MoveDirection.FORWARD -> System.out.println("Tequila Sunset goes forward");
-      case MoveDirection.BACKWARD -> System.out.println("Tequila Sunset goes backward");
-      case MoveDirection.RIGHT -> System.out.println("Tequila Sunset turns right");
-      case MoveDirection.LEFT -> System.out.println("Tequila Sunset turns left");
+    BaseWorldMap map = new BaseWorldMap(20,20, 10);
+    System.out.println(map);
+    for(int i = 0; i< 20; i++){
+      map.growPlantsAtRandomPositions(10);
     }
   }
 }
