4f4f972fb8f575f76edd4abd9e47591d96a87260
Massny <pawel.fornagiel2@gmail.com>
2025-01-19 22:17:06 +0100

    add worldMap tests, change gradle config

---
+++ src/test/java/agh/ics/oop/model/worldmap/ConfigTestSingleton.java
@@ -0,0 +1,40 @@
+package agh.ics.oop.model.worldmap;
+
+import agh.ics.oop.model.configuration.*;
+
+public class ConfigTestSingleton {
+
+  private static final ConfigMap mapConfig = new ConfigMap(
+      20,
+      20,
+      1000,
+      MapVariant.FIRES,
+      2,
+      4
+  );
+  private static final ConfigAnimal animalConfig = new ConfigAnimal(
+      30,
+      100,
+      6,
+      5,
+      2,
+      2,
+      3,
+      BehaviorVariant.CRAZINESS
+  );
+  private static final ConfigPlant plantConfig = new ConfigPlant(
+      5,
+      3,
+      4
+  );
+
+  public static ConfigMap getMapConfig() {
+    return ConfigTestSingleton.mapConfig;
+  }
+  public static ConfigAnimal getAnimalConfig() {
+    return ConfigTestSingleton.animalConfig;
+  }
+  public static ConfigPlant getPlantConfig() {
+    return ConfigTestSingleton.plantConfig;
+  }
+}
