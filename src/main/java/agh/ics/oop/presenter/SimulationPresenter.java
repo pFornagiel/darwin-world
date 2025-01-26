0225511873002fbefbe5b4a78759e395c9f0fd51
adriankrawczyk <adriankrawczyk0@o2.pl>
2025-01-26 13:52:09 +0100

    basic snail visualization

--- src/main/java/agh/ics/oop/presenter/SimulationPresenter.java
+++ src/main/java/agh/ics/oop/presenter/SimulationPresenter.java
@@ -24,6 +24,8 @@ import javafx.scene.chart.LineChart;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
@@ -100,7 +102,9 @@ public class SimulationPresenter implements MapChangeListener {
   private ConfigMap mapConfig;
   private ConfigPlant plantConfig;
   private ConfigAnimal animalConfig;
-
+  Image snailBack = new Image(getClass().getResourceAsStream("/snail_back.png"));
+  Image snailFront = new Image(getClass().getResourceAsStream("/snail_front.png"));
+  Image snailSide = new Image(getClass().getResourceAsStream("/snail_side.png"));
   private final Label[] ANIMAL_STATS_LABELS = new Label[8];
 
   @FXML
@@ -230,6 +234,12 @@ public class SimulationPresenter implements MapChangeListener {
   }
 
   private void drawAnimalElements(Iterable<Vector2d> positions, Vector2d offset, Vector2d size) {
+    if (snailBack.isError() || snailFront.isError() || snailSide.isError()) {
+      System.err.println("Failed to load snail images.");
+      return;
+    }
+    double cellSize = gridManager.calculateCellSize();
+
     for (Vector2d position : positions) {
       int x = position.getX() - offset.getX() + 1;
       int y = (position.getY() - offset.getY()) + 1;
@@ -241,30 +251,74 @@ public class SimulationPresenter implements MapChangeListener {
         continue;
       }
 
-      Color color = getAnimalColor(dataCollector.getAnimalData(currentAnimal),
-              simulationStatistics,
-              animalConfig.initialEnergy());
+      //Color color = getAnimalColor(dataCollector.getAnimalData(currentAnimal),
+       //       simulationStatistics,
+        //      animalConfig.initialEnergy());
 
       StackPane stackPane = new StackPane();
+      stackPane.setSnapToPixel(true);
       Rectangle cell = new Rectangle();
-      cell.setWidth(gridManager.calculateCellSize());
-      cell.setHeight(gridManager.calculateCellSize());
-      cell.setFill(color);
-      Text text = new Text(currentAnimal.toString());
-      text.setFill(Color.LIGHTGRAY);
-      text.setStyle(String.format("-fx-font-size: %fpx", gridManager.calculateCellSize()));
-      stackPane.getChildren().addAll(cell, text);
+      cell.setWidth(cellSize);
+      cell.setHeight(cellSize);
+      //cell.setFill(color);
+      cell.setFill(new Color(0,0,0,0));
+      String orientation = String.valueOf(currentAnimal.getOrientation());
+      ImageView snailImageView = new ImageView();
+      snailImageView.setFitWidth(cellSize);
+      snailImageView.setFitHeight(cellSize);
+      snailImageView.setPreserveRatio(true);
+      snailImageView.setSmooth(false);
+
+      double rotation = 0;
+
+      switch (orientation) {
+        case "North":
+          snailImageView.setImage(snailBack);
+          break;
+        case "South":
+          snailImageView.setImage(snailFront);
+          break;
+        case "East":
+          snailImageView.setImage(snailSide);
+          break;
+        case "West":
+          snailImageView.setImage(snailSide);
+          snailImageView.setScaleX(-1);
+          break;
+        case "North-East":
+          snailImageView.setImage(snailBack);
+          rotation = 45;
+          break;
+        case "North-West":
+          snailImageView.setImage(snailBack);
+          rotation = -45;
+          break;
+        case "South-East":
+          snailImageView.setImage(snailFront);
+          rotation = 45;
+          break;
+        case "South-West":
+          snailImageView.setImage(snailFront);
+          rotation = -45;
+          break;
+        default:
+          snailImageView.setImage(snailFront);
+          break;
+      }
+
+      snailImageView.setRotate(rotation);
+
+      stackPane.getChildren().addAll(cell, snailImageView);
 
       final Vector2d finalPosition = position;
       stackPane.setOnMouseClicked(event -> {
         chosenAnimal = getChosenAnimal(finalPosition, dataCollector.getAnimalsAtPosition(finalPosition));
         updateAnimalStatistics(chosenAnimal);
       });
-
+      gridPane.setSnapToPixel(true);
       gridPane.add(stackPane, x, y);
     }
   }
-
   private Animal getChosenAnimal(Vector2d position, List<Animal> animalList) {
     if (animalList.isEmpty()) {
       return null;
