7c0aa7266a09892d50faefda8d0a88aa9ce7a5b8
adriankrawczyk <adriankrawczyk0@o2.pl>
2025-01-27 20:14:53 +0100

    try catch for image

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module org.example.darwinworld {
   requires javafx.controls;
   requires javafx.fxml;
     requires com.google.gson;
+    requires java.desktop;
 
 
     opens agh.ics.oop to javafx.fxml;
