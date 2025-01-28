4d05f33fe13929b169839dbff00f017492de2a24
adriankrawczyk <adriankrawczyk0@o2.pl>
2025-01-28 16:10:04 +0100

    before color changes

--- src/main/java/agh/ics/oop/presenter/util/AnimalColor.java
+++ src/main/java/agh/ics/oop/presenter/util/AnimalColor.java
@@ -15,7 +15,7 @@ public class AnimalColor {
         List<Genotype> mostPopularGenotypes = stats.mostPopularGenotypes();
         double energyRatio = Math.min((double) animalData.energy() / animalEnergy, ONE);
         double r = 0.0, g = 0.0, b;
-        if (!mostPopularGenotypes.isEmpty() && animalData.genotype().equals(mostPopularGenotypes.getFirst())) {
+        if (!mostPopularGenotypes.isEmpty() && animalData.genotype().equals(mostPopularGenotypes)) {
             r = energyRatio * HALF + HALF;
             b = energyRatio * HALF + HALF;
             g = energyRatio * QUARTER;
